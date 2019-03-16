6ac480b8fd012d2284361f14809747a243416eb0
yanhao <yhzhu@umich.edu>
2019-03-16 15:54:08 -0400

    Add source code, data and cmake files

---
+++ src/kalman_filter.cpp
@@ -0,0 +1,89 @@
+#include "kalman_filter.h"
+#include <math.h>
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+/* 
+ * Please note that the Eigen library does not initialize 
+ *   VectorXd or MatrixXd objects with zeros upon creation.
+ */
+
+KalmanFilter::KalmanFilter() {}
+
+KalmanFilter::~KalmanFilter() {}
+
+void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
+                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+  x_ = x_in;
+  P_ = P_in;
+  F_ = F_in;
+  H_ = H_in;
+  R_ = R_in;
+  Q_ = Q_in;
+}
+
+void KalmanFilter::Predict() {
+  /**
+   * TODO: predict the state
+   */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
+}
+
+void KalmanFilter::Update(const VectorXd &z) {
+  /**
+   * TODO: update the state by using Kalman Filter equations
+   */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+}
+
+void KalmanFilter::UpdateEKF(const VectorXd &z) {
+  /**
+   * TODO: update the state by using Extended Kalman Filter equations
+   */
+  VectorXd z_pred(3);
+  z_pred[0] = sqrt(pow(x_[0],2) + pow(x_[1],2));
+  z_pred[1] = atan2(x_[1],x_[0]);
+  z_pred[2] = (x_[0]*x_[2] + x_[1]*x_[3]) / z_pred[0];
+  VectorXd y = z - z_pred;
+  
+  if (abs(x_[0])<1e-3 && abs(x_[1])<1e-3){
+    // If the point is too close to origin, atan2 doesn't give a reasonable value
+    // So we assume the error is zero
+    y[1] = 0;
+  }
+  else if (y[1] > M_PI){
+    // The difference in angle should never be greater than PI 
+    y[1] -= 2*M_PI;
+  }
+  else if (y[1] < -M_PI){
+    // The difference in angle should never be greater than PI 
+    y[1] += 2*M_PI;
+  }
+
+  MatrixXd Ht = H_.transpose();   // H_ is set to Hj in FusionEKF.cpp
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+}
